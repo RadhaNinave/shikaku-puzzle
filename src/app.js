3fce87134842e841fe33d6805ed0f7764b3bb85b
radhaCumulative <radhacumulative@gmail.com>
2026-09-25 11:13:35 +0530

    prepare project for vercel deployment

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(logger);
 

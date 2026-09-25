92d376b630b659f3089bd92465f4449304b343a9
radhaCumulative <radhacumulative@gmail.com>
2026-09-25 12:37:07 +0530

    prepare project for render

--- src/app.js
+++ src/app.js
@@ -1,4 +1,42 @@
 
+// import express from "express";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// import routes from "./routes/index.js";
+// import { logger } from "./middleware/logger.middleware.js";
+// import { errorHandler } from "./middleware/error.middleware.js";
+// import connectDB from "./config/db.js";
+
+// const app = express();
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+
+// app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, "../public")));
+
+// app.use(logger);
+
+// // Make sure MongoDB is connected before handling API requests
+// app.use(async (req, res, next) => {
+//   try {
+//     await connectDB();
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// app.use(routes);
+
+// app.use(errorHandler);
+
+// export default app;
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -6,7 +44,6 @@ import { fileURLToPath } from "url";
 import routes from "./routes/index.js";
 import { logger } from "./middleware/logger.middleware.js";
 import { errorHandler } from "./middleware/error.middleware.js";
-import connectDB from "./config/db.js";
 
 const app = express();
 
@@ -21,16 +58,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(logger);
-
-// Make sure MongoDB is connected before handling API requests
-app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
 
 app.use(routes);
 
